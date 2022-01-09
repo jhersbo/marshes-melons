a161fe21da4949e3eabb308df7b3085ad10d0ce3
jhersbo <jhersbo@gmail.com>
2022-01-08 21:41:36 -0600

    going to start building new auth code

---
+++ authv2.js
@@ -0,0 +1,12 @@
+// let inputs = {
+//     userRegistration: document.getElementById('username-input'),
+//     userRegPass1: document.getElementById('password1'),
+//     userRegPass2: document.getElementById('password2'),
+//     loginUser: document.getElementById('login-username'),
+//     loginPass: document.getElementById('login-password'),
+// }
+// let formButtons = {
+//     register: document.getElementById('register-submit'),
+//     login: document.getElementById('login-submit'),
+// }
+
